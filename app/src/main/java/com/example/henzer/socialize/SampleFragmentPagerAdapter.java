2d228acfa6d63b93b1c3b0ad0a20123a42c5efeb
Henzer <henzer.g.09@gmail.com>
2015-04-25 22:00:55 -0600

    Se agregó el proyecto nuevo
    Se agregaron todos los archivos.

---
+++ app/src/main/java/com/example/henzer/socialize/SampleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.henzer.socialize;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Henzer on 25/04/2015.
+ */
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Contactos", "Grupos", "Favoritos" };
+    private Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new Contacts();
+        //return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
