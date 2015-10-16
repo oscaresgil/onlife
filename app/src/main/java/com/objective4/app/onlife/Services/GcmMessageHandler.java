54fe3d37d8fe71580d10bf52354dcf789209a33e
fer <josefer1710@gmail.com>
2015-10-16 16:48:07 -0600

    icono blanco

--- app/src/main/java/com/objective4/app/onlife/Services/GcmMessageHandler.java
+++ app/src/main/java/com/objective4/app/onlife/Services/GcmMessageHandler.java
@@ -111,7 +111,7 @@ public class GcmMessageHandler extends IntentService {
     protected void onMessage(Context context) {
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_notification_icon)
+                        .setSmallIcon(R.drawable.ic_white_notification)
                         .setTicker(getResources().getString(R.string.notification_someone_block_you))
                         .setStyle(new NotificationCompat.BigTextStyle())
                         .setOnlyAlertOnce(true);
